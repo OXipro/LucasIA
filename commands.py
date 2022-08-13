faaf4bd4e6aa81c288197411773c6be17b8128d9
OXipro <97630250+OXipro@users.noreply.github.com>
2022-08-13 17:01:03 +0200

    update rajoute des nouvelles fonctions

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@ import datetime
 import pyttsx3
 import playsound
 from WeatherGetter import getWeather
+import vlc
 
 betaprefix = "lucas"
 tolerancy = 0
@@ -15,6 +16,8 @@ voices = voice.getProperty('voices')
 voice.setProperty('voice', voices[2].id)
 rate = voice.getProperty('rate')
 voice.setProperty('rate', 160)
+vlc = vlc.Instance()
+radio = vlc.media_player_new()
 
 
 class commands:
@@ -27,6 +30,7 @@ class commands:
             print("ok")
             voice.stop()
             voice.runAndWait()
+            radio.stop()
         if data.find(prefix + "merci") >= tolerancy:
             print("de rien")
             voice.say("de rien")
@@ -35,11 +39,22 @@ class commands:
             print("Bonjour " + UserName + " comment allez-vous ?")
             voice.say("Bonjour " + UserName + " comment tallez-vous ?")
             voice.runAndWait()
-        if data.find(prefix + "quelle heure est-il") >= tolerancy:
+        if data.find(prefix + "heure") >= tolerancy:
             print(datetime.datetime.now().strftime("Il est %H Heure et %M Minutes"))
             voice.say(datetime.datetime.now().strftime("Il est %H Heure et %M Minutes"))
             voice.runAndWait()
-        if data.find(prefix + "météo") >= tolerancy or data.find(prefix + "donne-moi la météo") >= tolerancy:
+        if data.find(prefix + "météo") >= tolerancy:
             print(getWeather())
             voice.say(getWeather())
             voice.runAndWait()
+        if data.find(prefix + "radio énergie") >= tolerancy:
+            w = vlc.media_new("http://streamingp.shoutcast.com/NRJ")
+            radio.set_media(w)
+            radio.play()
+        if data.find(prefix + "radio contact") >= tolerancy:
+            w = vlc.media_new("http://audiostream.rtl.be/contactfr192")
+            radio.set_media(w)
+            radio.play()
+        if data.find(prefix + "mais moins fort") >= tolerancy or data.find(prefix + "baisse le volume") >= tolerancy:
+            print("test")
+
